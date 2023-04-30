fdc2f28021deee468316d1d5150bb5d96b64ae70
karankashyap04 <karankashyap@gmail.com>
2023-04-30 05:45:08 -0400

    Working iterative texture transfer

---
+++ transfer.py
@@ -0,0 +1,247 @@
+from skimage.io import imread, imshow
+from skimage.color import rgb2lab, rgb2gray
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from typing import List
+
+# Declaring as global variable so we don't have to pass this as a function argument
+# (large size so saves space on stack --> enhances speed)
+texture_blocks = None
+
+def get_intensity_map(img: np.ndarray):
+    # return rgb2lab(img)[0] # return L channel values
+    return rgb2gray(img)
+
+def get_correspondence_function(key: str):
+    if key == "intensity": return get_intensity_map
+    elif not key:
+        raise Exception(f"{key} is not a valid correspondence function!")
+
+def get_texture_blocks(texture: np.ndarray, block_size: int):
+    print("get texture blocks")
+    th, tw, tc = texture.shape
+    global texture_blocks
+    texture_blocks = [texture[i:i+block_size, j:j+block_size,:] for j in range(tw - block_size) for i in range(th - block_size)]
+    print(len(texture_blocks))
+
+def get_random_block(blocks: List[np.ndarray]):
+    index = np.random.randint(len(blocks))
+    return blocks[index]
+
+def get_top_left_block(block_size: int, transfer_img_section: np.ndarray, correspondence: str):
+    print("get top left block")
+    correspondence_function = get_correspondence_function(correspondence)
+
+    transfer_correspondence_map = correspondence_function(transfer_img_section)
+    l2_norms = []
+    for block in texture_blocks:
+        block_correspondence_map = correspondence_function(block)
+        l2_norm = np.sum(np.square(block_correspondence_map - transfer_correspondence_map))
+        l2_norms.append(l2_norm)
+    # best_norm = min(l2_norms)
+    # tolerance = 0.1 * best_norm
+    # good_blocks = []
+    # for i, block in enumerate(texture_blocks):
+    #     if l2_norms[i] <= best_norm + tolerance:
+    #         good_blocks.append(block)
+    # return get_random_block(good_blocks)
+    best_norm_index = np.argmin(l2_norms)
+    return texture_blocks[best_norm_index]
+
+def find_good_block(transfer_result_segment: np.ndarray, transfer_img_segment: np.ndarray, correspondence: str, alpha: float, iter_num: int, overlap: int, row: int, col: int):
+    print("find good block")
+    # transfer_result_segment :: img_segment
+    # transfer_img_segment :: transfer_segment
+    h, w, c = transfer_result_segment.shape
+    downscaled_blocks = [np.copy(block)[:h, :w, :c] for block in texture_blocks]
+    correspondence_function = get_correspondence_function(correspondence)
+    transfer_img_segment_correspondence_map = correspondence_function(transfer_img_segment)
+    errors = []
+    for block in downscaled_blocks:
+        # overlap_error = np.sum(np.square((block - transfer_result_segment) * (transfer_result_segment > 0)))
+        overlap_error = 0
+        if row > 0: # there is overlap on top
+            overlap_error += np.sum(np.square(block[:overlap,:,:] - transfer_result_segment[:overlap,:,:]))
+        if col > 0: # there is overlap on the left
+            overlap_error += np.sum(np.square(block[:,:overlap,:] - transfer_result_segment[:,:overlap,:]))
+        if row > 0 and col > 0:
+            overlap_error -= np.sum(np.square(block[:overlap,:overlap,:] - transfer_result_segment[:overlap,:overlap,:]))
+        # overlap_error = np.sum(np.square(block[:overlap, :overlap, :] - transfer_result_segment[:overlap, :overlap, :]))
+        block_correspondence_map = correspondence_function(block)
+        correspondence_error = np.sum(np.square(block_correspondence_map - transfer_img_segment_correspondence_map))
+        if iter_num > 0:
+            previous_overlay_error = np.sum(np.square(block[overlap:, overlap:, :] - transfer_result_segment[overlap:, overlap:, :]))
+            error = (alpha * (overlap_error + previous_overlay_error)) + ((1 - alpha) * correspondence_error)
+        else:
+            error = (alpha * overlap_error) + ((1 - alpha) * correspondence_error)
+        errors.append(error)
+
+    # best_error = min(errors)
+    # tolerance = 0.1 * best_error
+    # good_blocks = []
+    # for i, block in enumerate(downscaled_blocks):
+    #     if errors[i] <= best_error + tolerance:
+    #         good_blocks.append(block)
+    # selected_block = get_random_block(good_blocks)
+    best_error_index = np.argmin(errors)
+    selected_block = downscaled_blocks[best_error_index]
+
+    error_map = np.sum(np.square(selected_block - transfer_result_segment) * (transfer_result_segment > 0), axis=-1)
+    return selected_block, error_map
+
+def min_err_boundary_cut(overlap_img: np.ndarray) -> np.ndarray:
+    print("min err boundary cut")
+    # Perform seamcarve
+    height, width = overlap_img.shape
+    for row in range(1, height):
+        for col in range(width):
+            if col == 0:
+                overlap_img[row,col] += min(overlap_img[row-1, col], overlap_img[row-1,col+1])
+            elif col == width - 1:
+                overlap_img[row,col] += min(overlap_img[row-1, col], overlap_img[row-1,col-1])
+            else:
+                overlap_img[row,col] += min(overlap_img[row-1,col-1], overlap_img[row-1, col], overlap_img[row-1,col+1])
+    
+    # Get the path
+    mask = np.zeros((height, width))
+    min_idx = 0
+    for i in range(height-1, -1, -1):
+        if i == height - 1:
+            min_idx = np.argmin(overlap_img[i,:])
+            row_mask = [1 for _ in range(min_idx)] + [0 for _ in range(width - min_idx)]
+            mask[i,:] = np.array(row_mask)
+        else:
+            if min_idx == 0:
+                check_vals = overlap_img[i,min_idx:min_idx+2]
+            elif min_idx == width - 1:
+                check_vals = overlap_img[i, min_idx-1:]
+            else:
+                check_vals = overlap_img[i, min_idx-1:min_idx+2]
+            if min_idx != 0:
+                min_idx -= 1
+            min_idx = min_idx + np.argmin(check_vals)
+            row_mask = [1 for _ in range(min_idx)] + [0 for _ in range(width - min_idx)]
+            mask[i,:] = np.array(row_mask)
+    return mask.astype(np.uint8)
+
+
+def transfer(block_size: int, texture_img: np.ndarray, transfer_img: np.ndarray, alpha: float, iter_num: int, correspondence: str, prev_result: np.ndarray):
+    th, tw, tc = texture_img.shape
+    outh, outw, outc = transfer_img.shape
+
+    if not (0 < block_size < th and 0 < block_size < tw):
+        print(f"Block size must be between 0 and texture min dimension; {block_size} not within [0, {min(th, tw)}]")
+        exit(1)
+    
+    # transfer_result = -100 * np.ones_like(texture_img)
+    transfer_result = prev_result
+    get_texture_blocks(texture_img, block_size)
+
+    transfer_result[:block_size, :block_size, :] = get_top_left_block(block_size, transfer_img[:block_size, :block_size, :], correspondence)
+    
+    overlap = math.ceil(block_size / 6)
+    for row in range(0, outh, block_size - overlap):
+        remainingY = outh - row
+        print(row)
+        for col in range(0, outw, block_size - overlap):
+            if row == 0 and col == 0:
+                continue
+            remainingX = outw - col
+            
+            transfer_result_segment = transfer_result[row: row + min(block_size, remainingY), col: col + min(block_size, remainingX)]
+            transfer_img_segment = transfer_img[row: row + min(block_size, remainingY), col: col + min(block_size, remainingX)]
+            selected_block, overlap_error = find_good_block(transfer_result_segment, transfer_img_segment, correspondence, alpha, iter_num, overlap, row, col)
+            # print(selected_block)
+            # imshow(selected_block)
+            # plt.show()
+
+            if row == 0: # overlap only on the left
+                # print("segment step 0")
+                # imshow(np.uint8(transfer_result_segment))
+                # plt.show()
+                overlap_error = overlap_error[:, :overlap]
+                mask = min_err_boundary_cut(overlap_error)
+                # print("mask")
+                # print(mask)
+                mask = np.repeat(mask[:,:,np.newaxis], 3, axis=2)
+                transfer_result_segment[:,overlap:] = 0
+                transfer_result_segment[:,:overlap] *= mask
+                # print("segment step 1")
+                # imshow(np.uint8(transfer_result_segment))
+                # plt.show()
+                flipped_mask = mask + (mask != 1) - (mask == 1)
+                ragged_block = np.copy(selected_block)
+                ragged_block[:,:overlap] *= flipped_mask
+                transfer_result_segment += ragged_block
+                # print("segment step 3")
+                # imshow(np.uint8(transfer_result_segment))
+                # plt.show()
+            elif col == 0: # overlap only on top
+                overlap_error = overlap_error[:overlap,:]
+                mask = min_err_boundary_cut(overlap_error.T).T
+                mask = np.repeat(mask[:,:,np.newaxis], 3, axis=2)
+                transfer_result_segment[overlap:,:] = 0
+                transfer_result_segment[:overlap, :] *= mask
+                flipped_mask = mask + (mask != 1) - (mask == 1)
+                ragged_block = np.copy(selected_block)
+                ragged_block[:overlap, :] *= flipped_mask
+                transfer_result_segment += ragged_block
+            else: # overlap on top and left
+                overlap_error_left = overlap_error[:, :overlap]
+                mask_left = min_err_boundary_cut(overlap_error_left)
+                mask_left = np.repeat(mask_left[:,:,np.newaxis], 3, axis=2)
+                overlap_error_top = overlap_error[:overlap, :]
+                mask_top = min_err_boundary_cut(overlap_error_top.T).T
+                mask_top = np.repeat(mask_top[:,:,np.newaxis], 3, axis=2)
+                # previously empty (-100) image segment becomes 0
+                transfer_result_segment[overlap:, overlap:] = 0
+                # apply masks to img_segment
+                total_mask = np.zeros_like(transfer_result_segment)
+                total_mask[:, :overlap] += mask_left
+                total_mask[:overlap, :] += mask_top
+                total_mask[total_mask > 1] = 1
+                transfer_result_segment *= total_mask
+                total_mask_flipped = total_mask + (total_mask != 1) - (total_mask == 1)
+                ragged_block = np.copy(selected_block)
+                ragged_block *= total_mask_flipped.astype(np.uint8)
+                transfer_result_segment += ragged_block
+
+            if remainingX <= block_size:
+                break
+        if remainingY <= block_size:
+            break
+    # print("transfer result:")
+    # print(transfer_result)
+    # imshow(np.uint8(transfer_result))
+    imshow(transfer_result)
+    plt.show()
+    return transfer_result
+
+
+def iterative_transfer(block_size: int, texture_path: str, transfer_path: str, correspondence: str, num_iters: int=2):
+    # NOTE: removing alpha channel (if present)
+    texture_img = imread(texture_path)[:,:,:3] / 255.0
+    transfer_img = imread(transfer_path)[:,:,:3] / 255.0 if transfer_path else None
+
+    print(f"texture shape: {texture_img.shape}")
+    print(f"transfer shape: {transfer_img.shape}")
+
+    ## Iteratively running the transfer function:
+    transfer_result = np.zeros_like(transfer_img)
+    for iter_num in range(num_iters):
+        print(f"Iteration: {iter_num} of {num_iters-1}")
+        ALPHA = (0.8 * iter_num / (num_iters-1)) + 0.1
+        # block_size = int(block_size // (1 + 0.2 * iter_num))
+        # block_size = int(block_size * (0.67 ** iter_num))
+        if iter_num > 0:
+            block_size = int(block_size * 0.67)
+        print(f"block_size: {block_size}")
+        transfer_result = transfer(block_size, texture_img, transfer_img, ALPHA, iter_num, correspondence, transfer_result)
+
+    # imshow(np.uint8(transfer_result))
+    imshow(transfer_result)
+    plt.savefig('transfer_result.png')
+    plt.show()
+    return transfer_result
+
